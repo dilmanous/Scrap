b992382e44c4232db2161d66323fb4aeb7bccdf0
Mehryar <apwbd.d@gmail.com>
2016-02-08 02:03:31 -0500

    Create scrap.js

---
+++ scrap.js
@@ -0,0 +1,12 @@
+function checkPermutation (a , b)
+{
+  // Sort two strings and check for similarity: O(nlogn)
+  // For each char in one string, look for first occurence of it in the other and remove both O(n + n-1 + n-2 + n-3) = O(n^2) 
+  // Using another data structure, count the number of occurence of each character for each string, they should match.
+  if (a.length = 
+} ;
+
+function mergesort (a)
+{
+
+} ;
